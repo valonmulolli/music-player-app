c702699ad712cf9438eba8aecc800204cbd00be4
valonmulolli <valonmulolli@gmail.com>
2024-04-26 20:13:01 +0200

    tab icons

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,21 +1,66 @@
 import { colors, fontSize } from '@/constants/tokens';
+import { FontAwesome, MaterialCommunityIcons, Ionicons, FontAwesome6 } from '@expo/vector-icons';
+import { BlurView } from 'expo-blur';
 import { Tabs } from 'expo-router';
+import { StyleSheet } from 'react-native';
 
 const TabsNavigation = () => {
   return (
-    <Tabs screenOptions={{
-      tabBarActiveTintColor: colors.primary,
-      tabBarLabelStyle: {
-        fontSize: fontSize.xs,
-        fontWeight: '500',
-      },
-      headerShown: false,
-    }}
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: colors.primary,
+        tabBarLabelStyle: {
+          fontSize: fontSize.xs,
+          fontWeight: '500',
+        },
+        headerShown: false,
+        tabBarStyle: {
+          position: 'absolute',
+          borderTopLeftRadius: 20,
+          borderTopRightRadius: 20,
+          borderTopWidth: 0,
+          paddingTop: 8,
+        },
+        tabBarBackground: () => (
+          <BlurView
+            intensity={25}
+            style={{
+              ...StyleSheet.absoluteFillObject,
+              overflow: 'hidden',
+              borderTopLeftRadius: 20,
+              borderTopRightRadius: 20,
+            }}
+          />
+        ),
+      }}
     >
-      <Tabs.Screen name='favorites' />
-      <Tabs.Screen name='playlist' />
-      <Tabs.Screen name='(songs)' />
-      <Tabs.Screen name='artists' />
+      <Tabs.Screen
+        name='favorites'
+        options={{
+          title: 'Favorites',
+          tabBarIcon: ({ color }) => (
+            <FontAwesome name='heart' size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen name='playlist' options={{
+        title: 'Playlist',
+        tabBarIcon: ({ color }) => (
+          <MaterialCommunityIcons name='playlist-play' size={28} color={color} />
+        ),
+      }} />
+      <Tabs.Screen name='(songs)' options={{
+        title: 'Favorites',
+        tabBarIcon: ({ color }) => (
+          <Ionicons name='musical-note-sharp' size={24} color={color} />
+        ),
+      }} />
+      <Tabs.Screen name='artists' options={{
+        title: 'Favorites',
+        tabBarIcon: ({ color }) => (
+          <FontAwesome6 name='users-line' size={20} color={color} />
+        ),
+      }} />
     </Tabs>
   );
 };
